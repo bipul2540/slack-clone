2e4dbbc718fe7fba7dba47e5a13974f9fe9a51a3
bipul2540 <bipulkumar2540@gmail.com>
2021-01-23 11:25:20 +0530

    COMMIT

--- frontend/src/component/App.js
+++ frontend/src/component/App.js
@@ -2,15 +2,22 @@ import React from "react";
 import Header from "./Header";
 import "./../styleCss/App.css";
 import Sidebar from "./Sidebar";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Chat from "./Chat";
 
 const App = () => {
   return (
     <div className="app">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-        {/** React-router---> chat screens  */}
-      </div>
+      <BrowserRouter>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/room/:roomId" component={Chat} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
